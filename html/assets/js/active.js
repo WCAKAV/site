580b2c248a6ce732e7bb0b8ec24acb41b7d69b0f
WCAKAV <147258wyq@gmial.com>
2026-01-20 14:04:59 +0800

    first

---
+++ html/assets/js/active.js
@@ -0,0 +1,41 @@
+(function($){
+	"use strict";
+
+	// Header Sticky
+	$(window).on('scroll',function() {
+		if ($(this).scrollTop() > 120){  
+			$('.navbar-area').addClass("is-sticky");
+		}
+		else{
+			$('.navbar-area').removeClass("is-sticky");
+		}
+	});
+
+	// Popup Image
+	$('.popup-btn').magnificPopup({
+		type: 'image',
+		gallery: {
+			enabled: true,
+		}
+	});
+
+	// Mean Menu
+	$('.mean-menu').meanmenu({
+		meanScreenWidth: "992"
+	});
+
+	// Go to Top
+	$(function(){
+		// Scroll Event
+		$(window).on('scroll', function(){
+			var scrolled = $(window).scrollTop();
+			if (scrolled > 600) $('.go-top').addClass('active');
+			if (scrolled < 600) $('.go-top').removeClass('active');
+		});  
+		// Click Event
+		$('.go-top').on('click', function() {
+			$("html, body").animate({ scrollTop: "0" },  500);
+		});
+	});
+
+}(jQuery));
